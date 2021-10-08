d8d2991ec0a6f63d75e3073afb7753d774b3766f
Chaoching Vang <chaoching.vang@gmail.com>
2021-10-08 16:45:57 -0500

    created fx to select only one operator button by adding class

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -13,18 +13,82 @@ function jqReady() {
 
 function addBtnSelected() {
     console.log(`in addBtnSelected fx`);
+    if ($(`#addBtn`).hasClass(`selectedOperator`)) {
+        $(`#addBtn`).removeClass(`selectedOperator`);
+    }
+    else {
+        if ($(`.selectedOperator`).length >= 1) {
+            console.log(`in else`);
+            $(`#addBtn`).removeClass(`selectedOperator`);
+            $(`#subtractBtn`).removeClass(`selectedOperator`);
+            $(`#multiplyBtn`).removeClass(`selectedOperator`);
+            $(`#divideBtn`).removeClass(`selectedOperator`);
+            $(`#addBtn`).addClass(`selectedOperator`);
+                }
+        else {
+            $(`#addBtn`).addClass(`selectedOperator`);
+        }
+    }
 }
 
 function subtractBtnSelected() {
     console.log(`in subtractBtnSelected fx`);
+    if ($(`#subtractBtn`).hasClass(`selectedOperator`)) {
+        $(`#subtractBtn`).removeClass(`selectedOperator`);
+    }
+    else {
+        if ($(`.selectedOperator`).length >= 1) {
+            console.log(`in else`);
+            $(`#addBtn`).removeClass(`selectedOperator`);
+            $(`#subtractBtn`).removeClass(`selectedOperator`);
+            $(`#multiplyBtn`).removeClass(`selectedOperator`);
+            $(`#divideBtn`).removeClass(`selectedOperator`);
+            $(`#subtractBtn`).addClass(`selectedOperator`);
+        }
+        else {
+            $(`#subtractBtn`).addClass(`selectedOperator`);
+        }
+    }
 }
 
 function multiplyBtnSelected() {
     console.log(`in multiplyBtnSelected fx`);
+    if ($(`#multiplyBtn`).hasClass(`selectedOperator`)) {
+        $(`#multiplyBtn`).removeClass(`selectedOperator`);
+    }
+    else {
+        if ($(`.selectedOperator`).length >= 1) {
+            console.log(`in else`);
+            $(`#addBtn`).removeClass(`selectedOperator`);
+            $(`#subtractBtn`).removeClass(`selectedOperator`);
+            $(`#multiplyBtn`).removeClass(`selectedOperator`);
+            $(`#divideBtn`).removeClass(`selectedOperator`);
+            $(`#multiplyBtn`).addClass(`selectedOperator`);
+        }
+        else {
+            $(`#multiplyBtn`).addClass(`selectedOperator`);
+        }
+    }
 }
 
 function divideBtnSelected() {
     console.log(`in divideBtnSelected fx`);
+    if ($(`#divideBtn`).hasClass(`selectedOperator`)) {
+        $(`#divideBtn`).removeClass(`selectedOperator`);
+    }
+    else {
+        if ($(`.selectedOperator`).length >= 1) {
+            console.log(`in else`);
+            $(`#addBtn`).removeClass(`selectedOperator`);
+            $(`#subtractBtn`).removeClass(`selectedOperator`);
+            $(`#multiplyBtn`).removeClass(`selectedOperator`);
+            $(`#divideBtn`).removeClass(`selectedOperator`);
+            $(`#divideBtn`).addClass(`selectedOperator`);
+        }
+        else {
+            $(`#divideBtn`).addClass(`selectedOperator`);
+        }
+    }
 }
 
 function calculateOperation() {
@@ -34,4 +98,3 @@ function calculateOperation() {
 function clearInputs() {
     console.log(`in clearInputs fx`);
 }
-
